2a28a690ba304b9bf1e3a20dd987982083e5ae7a
GDUNO2k <zzhaduongprozz@gmail.com>
2023-07-21 22:03:31 +0700

    header ui

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="">
+        <a href="#">
+          <img src="./default_user.png" />
+        </a>
+      </div>
+      <div className="header_button">
+        <button className="btn btn-primary">Schedule</button>
+        <button className="btn btn-primary">Social</button>
+        <button className="btn btn-primary">Messages</button>
+      </div>
+      <div className="">
+        <a href="#1">Name 1</a>
+        <i></i>
+        <a href="#2">Name 2</a>
+      </div>
+      <div className="notiication">
+        <i></i>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
